f3e3fb8ed6c5ee43921ce222c3852d40953178d1
Tejal Patel <tpatel@tejals-macbook-pro.local>
2019-01-18 23:44:57 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Tejal Patel on 1/18/19.
+//  Copyright © 2019 Tejal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
