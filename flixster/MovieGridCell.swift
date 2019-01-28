0930fc9a5786f36e65c2180bd61bc054addc8721
Tejal Patel <tpatel@tejals-macbook-pro.local>
2019-01-27 19:25:01 -0800

    Part two of flix project complete

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Tejal Patel on 1/27/19.
+//  Copyright © 2019 Tejal Patel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
